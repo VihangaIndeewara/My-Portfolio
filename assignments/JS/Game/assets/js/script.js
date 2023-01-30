08d2334bbed2543c0cf56a3bfc158ea8854cb7a4
VihangaIndeewra <indeewravihnga@gmail.com>
2023-01-30 16:36:36 +0530

    Added script variables

---
+++ assignments/JS/Game/assets/js/script.js
@@ -0,0 +1,16 @@
+
+var boy = document.getElementById("boy");
+
+var idleImageNumber = 1;
+var idleAnimationNumber = 0;
+var runImageNumber = 1;
+var runAnimationNumber = 0;
+var backgroundPositionX = 0;
+var moveBackgroundAnimationID = 0;
+var jumpImageNumber = 1;
+var jumpAnimationNumber = 0;
+var boyMarginTop = 567;
+var boxAnimationID = 0;
+var deadImageNumber = 1;
+var deadAnimationNumber = 0;
+var score = 0;
